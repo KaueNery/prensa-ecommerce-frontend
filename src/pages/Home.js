d2bc4306e33c99ca6a26070e4ffa52f1e65dd510
Kaue Nery <kauenery@Kaues-MacBook-Pro.local>
2022-04-20 21:55:25 -0300

    video settings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
 import CategoryList from "../components/category/CategoryList";
-import video from "../images/video.mp4";
+// import video from "../images/video.mp4";
 
 const Home = () => {
 
@@ -13,13 +13,13 @@ const Home = () => {
       
       <div className="container-fluid" >
         <div className="row">
-        <video loop autoPlay>
+        {/* <video loop autoPlay>
             <source
               src={video}
               type="video/mp4"
             />
             Your browser does not support the video tag.
-        </video> 
+        </video>  */}
        </div>
      
     </div>
